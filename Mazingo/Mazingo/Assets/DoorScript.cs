797f4837d3dbb45986b34f0882a5f08153b637b7
Per Josefsen <perj@itu.dk>
2018-04-13 23:31:30 +0200

    Updates

---
+++ Mazingo/Mazingo/Assets/DoorScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    public float openTime = 2;
+    public bool requireKey = false;
+    public GameObject key;
+    public Transform openPosition;
+
+
+    private Vector3 closedPosition;
+    private bool hasKey;
+    private float movingTime;
+
+    private bool openDoor;
+    private bool closeDoor;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        closedPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (openDoor && transform.position != openPosition.position)
+            movingTime += Time.deltaTime;
+        else if (closeDoor && transform.position != openPosition.position)
+            movingTime -= Time.deltaTime;
+        if (movingTime > 1)
+            movingTime = 1;
+        if (movingTime < 0)
+            movingTime = 0;
+        transform.position = Vector3.Lerp(closedPosition, openPosition.position, movingTime);
+    }
+
+    private void OnTriggerEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if ((requireKey && hasKey) || !requireKey)
+            {
+                closeDoor = false;
+                openDoor = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            closeDoor = true;
+            openDoor = false;
+        }
+    }
+}
