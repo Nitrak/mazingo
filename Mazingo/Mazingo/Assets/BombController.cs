edb3e9cfb0ce5627653d01941f22e8387cc8490c
Per Josefsen <perj@itu.dk>
2018-04-14 01:45:50 +0200

    Stuff

---
+++ Mazingo/Mazingo/Assets/BombController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour {
+
+    public float detonationTimeWhenCarried = 20;
+
+    public AudioClip tickingSound;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
