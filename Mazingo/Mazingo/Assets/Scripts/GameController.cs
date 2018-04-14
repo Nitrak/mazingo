0c7475355a8a9bb1d63a2f4b3e114cc8aa81b656
Per Josefsen <perj@itu.dk>
2018-04-14 17:12:10 +0200

    GameManager

---
+++ Mazingo/Mazingo/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public float Tick = 0.5f;
+    private float timer;
+    Assets.Scripts.Engine.RoomController rc;
+
+
+	// Use this for initialization
+	void Start () {
+        rc = new Assets.Scripts.Engine.RoomController();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer<=Tick)
+        {
+            timer = timer % Tick;
+            rc.Load();
+        }
+	}
+}
