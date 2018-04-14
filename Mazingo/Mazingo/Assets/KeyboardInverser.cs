6e4e005c36d37a889e4db6054d18b325845176e1
Per Josefsen <perj@itu.dk>
2018-04-14 19:50:39 +0200

    keyinverser

---
+++ Mazingo/Mazingo/Assets/KeyboardInverser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class KeyboardInverser : MonoBehaviour {
+
+
+    private FirstPersonController playerMoveSettings;
+
+    // Use this for initialization
+    void Start () {
+        var p = GameObject.FindGameObjectWithTag("Player");
+        playerMoveSettings = p.GetComponent<FirstPersonController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerMoveSettings.RandomizeControls();
+            Debug.Log("HER");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            playerMoveSettings.NormalizeControls();
+    }
+
+}
