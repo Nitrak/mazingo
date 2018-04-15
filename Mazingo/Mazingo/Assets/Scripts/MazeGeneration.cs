9740e664d48bf444ccc89a473c767f1e9373d6b7
MathiasNC <mak@netcompany.com>
2018-04-15 09:44:07 +0200

    last?

--- Mazingo/Mazingo/Assets/Scripts/MazeGeneration.cs
+++ Mazingo/Mazingo/Assets/Scripts/MazeGeneration.cs
@@ -370,7 +370,7 @@ namespace Assets.Scripts
             var rng = new Random();
             var seed = rng.Next(int.MaxValue);
             Debug.Log(seed);
-            rng = new Random(seed);
+            rng = new Random(106520481);
             var maze = new Maze();
             
 
